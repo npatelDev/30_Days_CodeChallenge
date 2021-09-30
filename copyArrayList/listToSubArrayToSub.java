7d8937f8bbb98fef0d8d06b965042c25ce8ba0ad
npatelDev <91616869+npatelDev@users.noreply.github.com>
2021-09-30 05:01:51 -0700

    30/09/2021

--- copyArrayList/listToSubArrayToSub.java
+++ copyArrayList/listToSubArrayToSub.java
@@ -1,1 +1,29 @@
+package com.nitesh1.ArrayListToSubList;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class listToSubArrayToSub {
+
+	public static void main(String[] args) {
+		
+			ArrayList<String> lista=new ArrayList<>();
+			lista.add("Hari");
+			lista.add("Mohan");
+			lista.add("Prasad");
+			lista.add("Monty");
+			lista.add("Mayank");
+			lista.add("Raju");
+			
+			System.out.println(lista);
+			ArrayList<String> listb=new ArrayList<>(lista.subList(1, 3));
+				System.out.println("SubList Stored in ArrayList :"+ listb);
+		
+				List<String> listc= new ArrayList<>(lista.subList(2, 4));
+			System.out.println("SubList Stored in List :"+listc);
+		}
+	
+
+
+	}
 
